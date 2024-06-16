e6f25e480d7e7a35d726781d4c9da7fbf168dc4c
CrispyLemon <aryanb349@gmail.com>
2024-06-16 08:11:56 +0530

    finished till 3.21

--- models/person.js
+++ models/person.js
@@ -24,7 +24,6 @@ const personSchema = new mongoose.Schema({
         validator: function(v){
           return  /\d{2,3}-\d+  /.test(v);
         },
-              message: props => `${props.value} is not a valid phone number!`
       },
       minLength:8,
       required:true 
